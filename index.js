93964edffd8e2615e7b73d849643a633997fa026
Emmeline Costin <emmecostin@gmail.com>
2021-09-15 10:16:03 -0700

    added logo and fixed errors

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 /* NavBar Function */
 
+$("#sec-1").click(function() {
+    $('html, body').animate({
+        scrollTop: 
+        $("#one").offset().top-100000
+    }, 1000);
+ return false;
+});
+
+$("#sec-2").click(function() {
+    $('html, body') .animate({
+        scrollTop:        $("#two").offset().top-112
+    }, 1000);
+ return false;
+});
+
+$("#sec-3").click(function() {
+    $(' html,body') .animate({
+        scrollTop:        $("#three").offset().top-112
+    }, 1000);
+ return false;
+});
+
+$("#sec-4").click(function() {
+ $(this).addClass("active");
+    $('html,body ') .animate({
+        scrollTop:        $("#four").offset().top-112
+    }, 1000);
+ return false;
+});
 /* IMAGES HOVER FUNCTION */
 
 $(document).ready(function() {
