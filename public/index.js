e0af8450492c7d4d0899cfb8e506d09b7510232b
Emmeline Costin <emmecostin@gmail.com>
2021-10-25 15:12:47 -0700

    cleaned up code

--- public/index.js
+++ public/index.js
@@ -1,37 +1,4 @@
-/* NavBar Function */
-
-$("#sec-1").click(function() {
-    $('html, body').animate({
-        scrollTop: 
-        $("#one").offset().top-100000
-    }, 1000);
- return false;
-});
-
-$("#sec-2").click(function() {
-    $('html, body') .animate({
-        scrollTop:        $("#two").offset().top-112
-    }, 1000);
- return false;
-});
-
-$("#sec-3").click(function() {
-    $(' html,body') .animate({
-        scrollTop:        $("#three").offset().top-112
-    }, 1000);
- return false;
-});
-
-$("#sec-4").click(function() {
- $(this).addClass("active");
-    $('html,body ') .animate({
-        scrollTop:        $("#four").offset().top-112
-    }, 1000);
- return false;
-});
-
-
-/* IMAGES HOVER FUNCTION */
+/* hover over images */
 
 $(document).ready(function() {
     $('#ezpresso').hover(function() {
@@ -59,30 +26,12 @@ $(document).ready(function() {
 
 // Moble Navbar Function 
 
-function navDropDown() {
-    var navDrop = document.getElementById("myLinks");
-    if (navDrop.style.display === "block") {
-      navDrop.style.display = "none";
-    } else {
-      navDrop.style.display = "block";
-    }
-  }
-
-/* AJAX */
-
-function loadRepo() {
-    const xhttp = new XMLHttpRequest(); 
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            results = JSON.parse(this.responseText);
-            results.forEach(repo=>{
-                document.getElementById("repo").innerHTML+="<li>"+repo.name+"</li>";
-            })
-          //  document.getElementById("repo").innerHTML = results[0].name;
-        }
-    }
-    xhttp.open("GET", "https://api.github.com/users/EMMELINECOSTIN/repos", true);
-    xhttp.send();
-}
-
+// function navDropDown() {
+//     var navDrop = document.getElementById("myLinks");
+//     if (navDrop.style.display === "block") {
+//       navDrop.style.display = "none";
+//     } else {
+//       navDrop.style.display = "block";
+//     }
+//   }
 
